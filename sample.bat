e061ffcce68c7d269968ac048ea4cd6367cc4c28
Narek1444 <nareksargsyan950@gmail.com>
2023-02-04 15:22:21 +0400

    fdasfads

--- sample.bat
+++ sample.bat
@@ -1,4 +1,6 @@
-git add .
-git commit -m "fdasfads"
-git push origin VGP-99
-gh pr create -t "Added some new text" -b "Added some text 1"
+@REM git add .
+@REM git commit -m "fdasfads"
+@REM git push origin VGP-99
+@REM gh pr create -t "Added some new text"
+
+echo The name of this file is: %~n0%~x0
