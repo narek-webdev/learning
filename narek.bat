5241bc9612b41d53b636f9202849192873461335
Narek1444 <nareksargsyan950@gmail.com>
2023-02-05 20:13:25 +0400

    narek

--- narek.bat
+++ narek.bat
@@ -1,7 +1,8 @@
 @echo off
 set commitName="%~n0"
+set prTitle="Pull request name"
 
 git add .
 git commit -m %commitName%
 git push
-gh pr create -t %commitName% -b ""
+gh pr create -t %prTitle% -b ""
